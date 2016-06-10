c9bfc704ea0c516ae2a86ddad0b388bed4170c49
RizzoV <valentino.rizzo.93@gmail.com>
2016-06-10 19:00:22 +0200

    Copia da progetto Client

--- client/MainWindow.xaml.cs
+++ client/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,16 +14,31 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace client
+namespace WpfApplication1
 {
-    /// <summary>
-    /// Logica di interazione per MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        byte[] buffer = new byte[1024];
+        Socket sock;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Inizialmente nascondi bottoni inutilizzabili senza connessione
+            buttonDisconnetti.IsEnabled = false;
+            buttonComando.IsEnabled = false;
+        }
+
+        private void buttonConnetti_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonDisconentti_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
