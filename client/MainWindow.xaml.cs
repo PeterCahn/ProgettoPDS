61922e6dcc2751c045b778be8463311d2be3800f
RizzoV <valentino.rizzo.93@gmail.com>
2016-06-10 23:24:57 +0200

    GUI modificata + Cattura ed invio comandi + Logger

--- client/MainWindow.xaml.cs
+++ client/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,19 +27,127 @@ namespace WpfApplication1
         {
             InitializeComponent();
 
-            // Inizialmente nascondi bottoni inutilizzabili senza connessione
+            // Inizialmente imposta bottoni come inutilizzabili senza connessione
             buttonDisconnetti.IsEnabled = false;
-            buttonComando.IsEnabled = false;
+            buttonInvia.IsEnabled = false;
+            buttonCattura.IsEnabled = false;
         }
 
         private void buttonConnetti_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                // Aggiorna stato
+                textBoxStato.AppendText("\nSTATO: In connessione...");
 
+                // Crea SocketPermission per richiedere l'autorizzazione alla creazione del socket
+                SocketPermission permission = new SocketPermission(NetworkAccess.Connect, TransportType.Tcp, textBoxIpAddress.Text, SocketPermission.AllPorts);
+
+                // Accertati di aver ricevuto il permesso, altrimenti lancia eccezione
+                permission.Demand();
+
+                /* // Interrogazione DNS, crea IPHostEntry contenente vettore di IPAddress di risultato          
+                IPHostEntry ipHost = Dns.GetHostEntry(textIpAddress.Text); */
+
+                // Ricava IPAddress da risultati interrogazione DNS
+                IPAddress ipAddr = IPAddress.Parse(textBoxIpAddress.Text);  
+
+
+                // Crea endpoint a cui connettersi
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 4510);
+
+                // Crea socket
+                sock = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                // Connettiti
+                sock.Connect(ipEndPoint);
+
+                // Aggiorna stato
+                textBoxStato.AppendText("\nSTATO: Connesso a " + sock.RemoteEndPoint.ToString() + ".");
+                textBoxStato.ScrollToEnd();
+
+                // Aggiorna bottoni
+                buttonDisconnetti.IsEnabled = true;
+                buttonCattura.IsEnabled = true;
+                buttonConnetti.IsEnabled = false;
+                textBoxIpAddress.IsEnabled = false;
+            }
+            catch (Exception exc) {
+                textBoxStato.AppendText("\nECCEZIONE: " + exc.ToString());
+                textBoxStato.ScrollToEnd();
+            }
         }
 
         private void buttonDisconentti_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                // Disabilita e chiudi socket 
+                sock.Shutdown(SocketShutdown.Both);
+                sock.Close();
 
+                // Aggiorna bottoni
+                buttonDisconnetti.IsEnabled = false;
+                buttonInvia.IsEnabled = false;
+                textBoxIpAddress.IsEnabled = true;
+                buttonConnetti.IsEnabled = true;
+
+                // Aggiorna stato
+                textBoxStato.AppendText("\nSTATO: Disconnesso.");
+                textBoxStato.ScrollToEnd();
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
         }
+
+        private void buttonInvia_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Invia messaggio 
+                string messaggio = textBoxComando.Text;
+                byte[] msg = Encoding.Unicode.GetBytes(messaggio + "<END>");
+                int NumDiBytesInviati = sock.Send(msg);
+
+                /* TODO: ricevi */
+
+                // Aggiorna bottoni e textBox
+                textBoxComando.Text = "";
+                buttonInvia.IsEnabled = false;
+                buttonCattura.Content = "Cattura Comando";
+                buttonCattura.IsEnabled = true;
+
+                // Rimuovi event handler per non scrivere più i bottoni premuti nel textBox
+                this.KeyDown -= new KeyEventHandler(OnButtonKeyDown);
+
+            }
+            catch (Exception exc) {
+                textBoxStato.AppendText("\nECCEZIONE: " + exc.ToString());
+                textBoxStato.ScrollToEnd();
+            }
+        }
+
+        private void buttonCattura_Click(object sender, RoutedEventArgs e)
+        {
+
+            buttonCattura.Content = "In cattura...";
+            buttonCattura.IsEnabled = false;
+
+            // Crea event handler per scrivere i tasti premuti
+            this.KeyDown += new KeyEventHandler(OnButtonKeyDown);
+        }
+
+        private void OnButtonKeyDown(object sender, KeyEventArgs e)
+        {
+            if (textBoxComando.Text.Length == 0) {
+                textBoxComando.Text = e.Key.ToString();
+                buttonInvia.IsEnabled = true;
+            }
+            else {
+                if (!textBoxComando.Text.Contains(e.Key.ToString()))
+                    textBoxComando.AppendText("+" + e.Key.ToString());
+            }
+        }
+
+        
     }
 }
